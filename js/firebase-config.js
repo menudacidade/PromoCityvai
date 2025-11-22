1be4060da0ece3b85c79ddb6f7629a5b122922d2
menudacidade <menudacidade0@gmail.com>
2025-11-22 14:00:41 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // CONFIGURAÇÃO DO FIREBASE (REALTIME DATABASE)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI", // <--- COLAR AQUI
+  apiKey: "SUA_API_KEY_AQUI", // <--- COLE AQUI A SUA API KEY
   authDomain: "promocityvai-e278a.firebaseapp.com",
   databaseURL: "https://promocityvai-e278a-default-rtdb.firebaseio.com", // Seu banco!
   projectId: "promocityvai-e278a",
   storageBucket: "promocityvai-e278a.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  messagingSenderId: "SEU_SENDER_ID_AQUI", // <--- COLE AQUI O SEU SENDER ID
+  appId: "SEU_APP_ID_AQUI" // <--- COLE AQUI O SEU APP ID
 };
 
 // Inicializa Firebase
